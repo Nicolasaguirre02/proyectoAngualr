20b98ac31e9b641ebec73282008ad70c0e475820
nicolas aguirre <nico.aguirre02@hotmail.com>
2023-12-31 15:58:30 -0300

    creacion de modulo layout y  dos componentes (navbar y toolbar)

--- src/app/componentes/layout/layout.module.ts
+++ src/app/componentes/layout/layout.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent,
+    ToolbarComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports:[
+    NavbarComponent,
+    ToolbarComponent
   ]
 })
 export class LayoutModule { }
